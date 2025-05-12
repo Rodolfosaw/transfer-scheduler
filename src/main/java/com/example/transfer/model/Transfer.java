44bcbed30d503ebf4c68c4f440b8d52722634709
Rodolfo Medeiros <57359918+Rodolfosaw@users.noreply.github.com>
2025-05-12 17:18:48 -0300

    Update Transfer.java

--- src/main/java/com/example/transfer/model/Transfer.java
+++ src/main/java/com/example/transfer/model/Transfer.java
@@ -1,14 +1,15 @@
 package com.example.transfer.model;
 
+import lombok.Data;
 import javax.persistence.*;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
 @Entity
+@Data
 public class Transfer {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String originAccount;
     private String destinationAccount;
     private BigDecimal amount;
