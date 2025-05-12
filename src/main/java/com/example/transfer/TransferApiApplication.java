2e5ae0f30ef8f19caa7871c619c69c4c54af3e26
Rodolfo Medeiros <57359918+Rodolfosaw@users.noreply.github.com>
2025-05-12 16:24:38 -0300

    Create TransferApiApplication.java

---
+++ src/main/java/com/example/transfer/TransferApiApplication.java
@@ -0,0 +1,11 @@
+package com.example.transfer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransferApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TransferApiApplication.class, args);
+    }
+}
