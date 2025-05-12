1f0849bf62a23028bcf5f113e5414ca1066b6b10
Rodolfo Medeiros <57359918+Rodolfosaw@users.noreply.github.com>
2025-05-12 16:35:30 -0300

    Create TransferRepository.java

---
+++ src/main/java/com/example/transfer/repository/TransferRepository.java
@@ -0,0 +1,6 @@
+package com.example.transfer.repository;
+
+import com.example.transfer.model.Transfer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TransferRepository extends JpaRepository<Transfer, Long> {}
